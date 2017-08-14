4cf2e2264813e6f364750d66fda13d9e7766460e
abmuiz <abmuiz123@gmail.com>
2017-08-14 13:33:39 +0530

    Update index.php
    echoed connection established

--- index.php
+++ index.php
@@ -2,3 +2,5 @@
 
 $conn = new PDO("mysql:host=localhost;dbname=company","root","tamboli9214
     ");
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
